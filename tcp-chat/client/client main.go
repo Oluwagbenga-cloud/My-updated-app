1ae47048f869f20183105d9d53fcb7e63c5528bf
oluwagbenga oyewole <gbengaoyewole89@gmail.com>
2022-03-15 00:25:56 +0100

    My chat app

--- tcp-chat/client/client main.go
+++ tcp-chat/client/client main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
+	"io"
 	"net"
 	"os"
 	"strings"
@@ -11,16 +11,18 @@ import (
 
 func logFatal(err error) {
 	if err != nil {
-		log.Fatal(err)
+		logFatal(err)
 	}
 
 }
 
 func main() {
 	connection, err := net.Dial("tcp", "localhost:8080")
-	log.Fatal(err)
+	logFatal(err)
 
 	defer connection.Close()
+
+	fmt.Println("Enter your username :")
 
 	reader := bufio.NewReader(os.Stdin)
 	username, err := reader.ReadString('\n')
@@ -29,8 +31,38 @@ func main() {
 
 	username = strings.Trim(username, "\r\n")
 
-	welcomeMsg := fmt.Sprintf("Welcome %s, to the chat, Hi Deca Gophers.", username)
+	welcomeMsg := fmt.Sprintf("Welcome %s, to the chat, Hi Decagon Squad 10 Gophers.\n", username)
 
 	fmt.Println(welcomeMsg)
 
+	go read(connection)
+	write(connection, username)
+}
+func read(connection net.Conn) {
+	for {
+		reader := bufio.NewReader(connection)
+		message, err := reader.ReadString('\n')
+
+		if err == io.EOF {
+			connection.Close()
+			fmt.Println("Connection closed")
+			os.Exit(0)
+		}
+		fmt.Println(message)
+		fmt.Println("---------------------------------------- ")
+	}
+}
+
+func write(connection net.Conn, username string) {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		message, err := reader.ReadString('\n')
+
+		if err != nil {
+			break
+		}
+		message = fmt.Sprintf("%s:-%s\n", username, strings.Trim(message, "\r\n"))
+
+		connection.Write([]byte(message))
+	}
 }
